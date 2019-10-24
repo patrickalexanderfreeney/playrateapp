e1a794c77a691d38dd6502dde0a3e2c5d0da3a0a
Patrick Alexander <patrick_alexander@mail.com>
2019-10-24 17:11:17 -0500

    patrick changes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,21 +5,23 @@ class ReviewsController < ApplicationController
     end
 
     def new 
-        @review = review.new
-        @songs = song.all
+        @review = Review.new
+        @songs = Song.all
     end
 
     def create
-
-        Review.create
-
-           redirect_to song_path
+        @user_id = session[:user_id]
+        @review = Review.create(review_params)
+        
+        # byebug
+        # @song = Review.find(song_id: @review.song_id)
+        redirect_to song_path(@song)
     end
 
     private
 
     def review_params
-        params.require(review).permit(:user_id, :song_id, :content, :rating)
+        params.require(:review).permit(:user_id, :song_id, :content, :rating)
     end
 
 end
